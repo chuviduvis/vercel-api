222c5c751af1e3a87a8fa1fed68d1d15508a1ebb
chuviduvis <antr17195@gmail.com>
2022-08-11 17:20:47 -0500

    2 commit

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ require('dotenv').config()
 const app = express()
 const bodyparser = require('body-parser')
 
-app.use(cors())
-app.use(bodyparser.json())
+// app.use(cors())
+// app.use(bodyparser.json())
 
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
 
-    const Body = JSON.stringify(req.body)
-    const options = {
-        method: 'POST',
-        body: `${Body}`
-    };
-    fetch(`https://api.spoonacular.com/food/ingredients/glycemicLoad?apiKey=${process.env.API_KEY}`, options)
-        .then(response => response.json())
-        .then (response => res.json(response))
-        .catch(err => console.error(err,));
+    // const Body = JSON.stringify(req.body)
+    // const options = {
+    //     method: 'POST',
+    //     body: `${Body}`
+    // };
+    // fetch(`https://api.spoonacular.com/food/ingredients/glycemicLoad?apiKey=${process.env.API_KEY}`, options)
+    //     .then(response => response.json())
+    //     .then (response => res.json(response))
+    //     .catch(err => console.error(err,));
 
 
-    // res.json('Hey this is my API running 🥳')
+    res.json('Hey this is my API running 🥳')
   })
 
 
