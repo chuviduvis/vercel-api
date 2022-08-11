f3cf4daf29aab85c94bc8bfe34c452ec6b09f1bf
chuviduvis <antr17195@gmail.com>
2022-08-11 18:03:52 -0500

    commit 5

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const app = express()
 const bodyparser = require('body-parser')
 
 app.use(cors())
-app.use(bodyparser.json())
+// app.use(bodyparser.json())
 
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    const Body = JSON.stringify(req.body)
-    const options = {
-        method: 'POST',
-        body: `${Body}`
-    };
-    fetch(`https://api.spoonacular.com/food/ingredients/glycemicLoad?apiKey=${process.env.API_KEY}`, options)
-        .then(response => response.json())
-        .then (response => res.json(response))
-        .catch(err => console.error(err,));
+app.get('/', (req, res) => {
+    // console.log(req.body)
+    // const Body = JSON.stringify(req.body)
+    // const options = {
+    //     method: 'POST',
+    //     body: `${Body}`
+    // };
+    // fetch(`https://api.spoonacular.com/food/ingredients/glycemicLoad?apiKey=${process.env.API_KEY}`, options)
+    //     .then(response => response.json())
+    //     .then (response => res.json(response))
+    //     .catch(err => console.error(err,));
     // const response = {
     //     status: 'success',
     //     totalGlycemicLoad: 6.64,
@@ -33,7 +33,7 @@ app.post('/', (req, res) => {
     //   }
     //   res.json(response)
 
-    // res.json('Hey this is my API running 🥳')
+    res.json('Hey this is my API running 🥳')
   })
 
 
