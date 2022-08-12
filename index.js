7c66539420ebd94603e39c094772d5cf728bf8d7
chuviduvis <antr17195@gmail.com>
2022-08-11 23:50:05 -0500

    commit 26

--- index.js
+++ index.js
@@ -4,26 +4,31 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express()
 const bodyparser = require('body-parser')
+const { json } = require('body-parser')
 
 app.use(cors())
 app.use(bodyparser.json())
 
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
     // console.log(req.body)
-  
+    // if(process.env.API_KEY === undefined){
+    //   objeto = {
+    //     noapikey: true
+    //   }
+    //   res.json(objeto)
 
-  
+    // }
     const options = {
         method: 'POST',
         body:`{ "ingredients":[ "1 apple" ] }`
     };
     fetch(`https://api.spoonacular.com/food/ingredients/glycemicLoad?apiKey=${process.env.API_KEY}`, options)
-        .then(response => {
-          return res.json(response)})
+        .then(response => response.json())
+        .then (response => res.json(response))
         .catch(err => {
-            return res.json(err)
-            console.error(err, "2")});
+            res.status(500),json({ error})
+            console.error(err)});
     // const response = {
     //     status: 'success',
     //     totalGlycemicLoad: 6.64,
@@ -37,7 +42,7 @@ app.post('/', (req, res) => {
     //   }
     //   res.json(response)
 
-    //   res.json('Hey this is my API running 🥳')
+    // res.json('Hey this is my API running 🥳')
   })
 
 
