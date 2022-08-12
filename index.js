15ed3544d91068ecb72ff3512029c49b6e3f0412
chuviduvis <antr17195@gmail.com>
2022-08-11 21:51:39 -0500

    commit 12

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ require('dotenv').config()
 const app = express()
 const bodyparser = require('body-parser')
 
-app.use(cors())
-app.use(bodyparser.json())
+// app.use(cors())
+// app.use(bodyparser.json())
 
 
 app.post('/', (req, res) => {
     // console.log(req.body)
-  
+    if(process.env.API_KEY === undefined){
+      objeto = {
+        noapikey: true
+      }
+      res.json(objeto)
+
+    }
     const options = {
         method: 'POST',
         body:`{ "ingredients":[ "1 apple" ] }`
@@ -20,7 +26,7 @@ app.post('/', (req, res) => {
         .then(response => response.json())
         .then (response => res.json(response))
         .catch(err => {
-            res.json(err, "1")
+            res.json(err)
             console.error(err, "2")});
     // const response = {
     //     status: 'success',
